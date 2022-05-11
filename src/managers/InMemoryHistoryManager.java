bbb3b361bb709e386d6138b3ca21dd90e21e021f
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2022-05-11 11:37:20 +0300

    style: исправить отступы в классе managers.InMemoryHistoryManager

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -9,8 +9,8 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final ArrayList<Task> history = new ArrayList<>();
 
     @Override
-    public void add(Task task){
-        if(history.size() > 10){
+    public void add(Task task) {
+        if (history.size() > 10) {
             history.remove(0);
         }
         history.add(task);
