762e3f52657359cff7d48beaa513ddb4de0bd5df
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2022-05-11 13:21:26 +0300

    refactor: изменить структуру данных истории на связный список

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -6,12 +6,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history = new LinkedList<>();
+    private final LinkedList<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (history.size() > 10) {
-            history.remove(0);
+            history.remove();
         }
         history.add(task);
     }
