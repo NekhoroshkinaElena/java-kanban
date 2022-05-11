7e5cd771f0be9abf309d9f37e038239beea0c934
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2022-05-11 12:37:54 +0300

    style: исправить отступы в классе managers.Managers

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -2,7 +2,7 @@ package managers;
 
 public class Managers {
 
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
